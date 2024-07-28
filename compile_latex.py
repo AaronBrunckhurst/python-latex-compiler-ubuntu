ab3f65efb74498dca5b9cd1b7782eaff7f418188
Aaron B <dbrunckh@ucsc.edu>
2024-07-28 16:17:36 -0700

    Made the compile latex script not install anything if it does not need to.

--- compile_latex.py
+++ compile_latex.py
@@ -2,25 +2,42 @@ import subprocess
 import os
 import sys
 
-def install_prerequisites():
-    """Install necessary LaTeX packages."""
+def is_package_installed(package_name):
+    """Check if a package is installed."""
     try:
-        subprocess.run(['sudo', 'apt-get', 'update'], check=True)
-        subprocess.run(['sudo', 'apt-get', 'install', '-y', 'texlive-latex-base', 'texlive-latex-recommended', 'texlive-latex-extra', 'texlive-fonts-recommended', 'texlive-fonts-extra', 'texlive-bibtex-extra'], check=True)
+        subprocess.run(['dpkg-query', '-W', '-f=${Status}', package_name], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+def install_prerequisites():
+    """Install necessary LaTeX packages if not already installed."""
+    packages = [
+        'texlive-latex-base', 'texlive-latex-recommended', 'texlive-latex-extra',
+        'texlive-fonts-recommended', 'texlive-fonts-extra', 'texlive-bibtex-extra'
+    ]
+    
+    try:
+        #subprocess.run(['sudo', 'apt-get', 'update'], check=True)
+        for package in packages:
+            if not is_package_installed(package):
+                subprocess.run(['sudo', 'apt-get', 'install', '-y', package], check=True)
+            else:
+                print(f"Package {package} is already installed.")
     except subprocess.CalledProcessError as e:
-        print("An error occurred while installing prerequisites: {}".format(e))
+        print(f"An error occurred while installing prerequisites: {e}")
         sys.exit(1)
 
 def compile_latex(tex_file):
     """Compile a .tex file into a .pdf file."""
     if not os.path.isfile(tex_file):
-        print("File not found: {}".format(tex_file))
+        print(f"File not found: {tex_file}")
         sys.exit(1)
     
     try:
         subprocess.run(['pdflatex', tex_file], check=True)
     except subprocess.CalledProcessError as e:
-        print("An error occurred while compiling the LaTeX document: {}".format(e))
+        print(f"An error occurred while compiling the LaTeX document: {e}")
         sys.exit(1)
 
 if __name__ == '__main__':
